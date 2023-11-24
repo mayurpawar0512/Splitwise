aca8add8c0c88a3bd6b6cf3ccb146eb0e3c8543b
Mayur <mayurpawar1512@gmail.com>
2023-11-24 21:43:51 +0530

    HeapBasedStrategy Algorithm completed

--- src/main/java/com/example/Splitwise/service/strategy/HeapBasedStrategy.java
+++ src/main/java/com/example/Splitwise/service/strategy/HeapBasedStrategy.java
@@ -45,9 +45,46 @@ public class HeapBasedStrategy implements SettleUpStrategy{
         }
 
         //Calculate transactions till one of them becomes zero
+        /*
+        * List of userExpense can have one user entry two times
+        * for eg A paid 300 in group of three
+        *
+        * A ->  300
+        * A -> -100 {A appeared twice)
+        * B -> -100
+        * C -> -100
+        *
+        *  but in the map there will be only one entry i.e. for each user their outstanding amount
+        *
+        * A ->  200
+        * B -> -100
+        * C -> -100
+        *
+        * size of map will be ALWAYS equal to number of users
+        * */
 
+        while(!minHeap.isEmpty()){
+            Map.Entry<User, Double> maxHasToPay = minHeap.poll();
+            Map.Entry<User, Double> maxWillGetPaid = maxHeap.poll();
 
+            TransactionDTO transactionDTO = new TransactionDTO(
+                    maxHasToPay.getKey().getName(),
+                    maxWillGetPaid.getKey().getName(),
+                    Math.min(Math.abs(maxHasToPay.getValue()), maxWillGetPaid.getValue())
+            );
 
+            transactions.add(transactionDTO);
+            double remainingBalance = maxHasToPay.getValue() + maxWillGetPaid.getValue();
+            if(remainingBalance == 0 ){
+                System.out.println("Settled !!");
+            }else if(remainingBalance < 0){
+                maxHasToPay.setValue(remainingBalance);
+                minHeap.add(maxHasToPay);
+            }else if(remainingBalance > 0){
+                maxWillGetPaid.setValue(remainingBalance);
+                minHeap.add(maxWillGetPaid);
+            }
+        }
 
         return transactions;
     }
