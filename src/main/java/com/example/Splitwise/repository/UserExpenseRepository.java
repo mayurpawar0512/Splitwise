27a9b7eabba86a46aed5d098e29f78bbcb150939
Mayur <mayurpawar1512@gmail.com>
2023-11-26 13:46:35 +0530

    Added repositories

---
+++ src/main/java/com/example/Splitwise/repository/UserExpenseRepository.java
@@ -0,0 +1,9 @@
+package com.example.Splitwise.repository;
+
+import com.example.Splitwise.model.UserExpense;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserExpenseRepository  extends JpaRepository<UserExpense, Integer> {
+}
---
+++ src/main/java/com/example/Splitwise/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.Splitwise.repository;
+
+import com.example.Splitwise.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
